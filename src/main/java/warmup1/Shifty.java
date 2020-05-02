41190df3fa25ede5bbc1fd27ae731f6f0695c849
pambrose <pambrose@mac.com>
2020-05-02 11:05:02 -0700

    Update content

---
+++ src/main/java/warmup1/Shifty.java
@@ -0,0 +1,22 @@
+package warmup1;
+
+import static com.github.pambrose.common.util.ArrayUtils.arrayPrint;
+
+public class Shifty {
+
+    public static int[] shifty(int[] vals) {
+
+        int[] results = new int[vals.length];
+
+        for (int i = 1; i < vals.length; i++)
+            results[i - 1] = vals[i];
+
+        results[results.length - 1] = vals[0];
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        arrayPrint(shifty(new int[]{1, 2, 3, 4, 5}));
+    }
+}
--- src/main/kotlin/Content2.kt
+++ src/main/kotlin/Content2.kt
@@ -1,37 +1,8 @@
-import com.github.readingbat.ReturnType
+import com.github.readingbat.ReturnType.*
 import com.github.readingbat.dsl.readingBatContent
 
 val content2 =
   readingBatContent {
-
-    kotlin {
-      repoRoot = "https://github.com/readingbat/readingbat-java-content"
-
-      group("Lambdas 1") {
-        packageName = "lambda1"
-        description = "This is a description of Lambdas 1"
-
-        challenge("StringLambda1") {
-          description = "This is a description of StringLambda1"
-
-          "" returns "0a"
-          "" returns "0a, 1b, 2c, 3d"
-        }
-
-        challenge("StringLambda2") {
-          description = "This is a description of StringLambda1"
-
-          "" returns "0g"
-          "" returns "0h, 1o, 2u, 3s, 4e"
-        }
-
-      }
-
-      group("Lambdas 2") {
-        packageName = "lambdas2"
-        description = "This is a description of Lambdas 2"
-      }
-    }
 
     java {
       repoRoot = "https://github.com/readingbat/readingbat-java-content"
@@ -43,19 +14,23 @@ val content2 =
         challenge("JoinEnds") {
           description = """This is a description of JoinEnds"""
           codingBatEquiv = "p141494"
-          returnType = ReturnType.StringType
+          returnType = StringType
         }
 
         challenge("StringLength") {
           description = """This is a description of StringLength"""
           codingBatEquiv = "p141494"
-          returnType = ReturnType.IntType
+          returnType = IntType
         }
 
         challenge("LookForIt") {
           description = """This is a description of LookForIt"""
-          returnType = ReturnType.BooleanType
+          returnType = BooleanType
+        }
 
+        challenge("Shifty") {
+          description = """This is a description of Shifty"""
+          returnType = IntArrayType
         }
       }
 
@@ -92,6 +67,35 @@ val content2 =
       group("Array 2") {
         packageName = "array2"
         description = "This is a description of Array 2"
+      }
+    }
+
+    kotlin {
+      repoRoot = "https://github.com/readingbat/readingbat-java-content"
+
+      group("Lambdas 1") {
+        packageName = "lambda1"
+        description = "This is a description of Lambdas 1"
+
+        challenge("StringLambda1") {
+          description = "This is a description of StringLambda1"
+
+          "" returns "0a"
+          "" returns "0a, 1b, 2c, 3d"
+        }
+
+        challenge("StringLambda2") {
+          description = "This is a description of StringLambda1"
+
+          "" returns "0g"
+          "" returns "0h, 1o, 2u, 3s, 4e"
+        }
+
+      }
+
+      group("Lambdas 2") {
+        packageName = "lambdas2"
+        description = "This is a description of Lambdas 2"
       }
     }
   }
