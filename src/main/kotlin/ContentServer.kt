befb4bec83577ac26837594d5997bea8e1f94e03
pambrose <pambrose@mac.com>
2020-05-10 21:54:20 -0700

    Update content

--- src/main/kotlin/ContentServer.kt
+++ src/main/kotlin/ContentServer.kt
@@ -3,6 +3,6 @@ import com.github.readingbat.ReadingBatServer
 object ContentServer {
   @JvmStatic
   fun main(args: Array<String>) {
-    ReadingBatServer.start(content)
+    ReadingBatServer.start(args)
   }
 }
