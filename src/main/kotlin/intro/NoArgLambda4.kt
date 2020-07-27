b91b1ff3ae9d622e4b7a4c6714254ee6686d9975
pambrose <pambrose@mac.com>
2020-07-27 13:25:45 -0700

    Update content

--- src/main/kotlin/intro/NoArgLambda4.kt
+++ src/main/kotlin/intro/NoArgLambda4.kt
@@ -5,13 +5,17 @@ fun funcGen1(i: Int): () -> Int {
   return { i }
 }
 
-fun funcNoArg4(i: Int, j: Int): Int {
+fun funcNoArg4a(i: Int, j: Int): Int {
   return i * funcGen1(j).invoke()
 }
 
+fun funcNoArg4b(i: Int, j: Int): Int {
+  return i * funcGen1(j)()
+}
+
 fun main() {
-  println(funcNoArg4(5, 10))
-  println(funcNoArg4(10, 15))
-  println(funcNoArg4(6, 12))
-  println(funcNoArg4(20, 25))
+  println(funcNoArg4a(5, 10))
+  println(funcNoArg4a(10, 15))
+  println(funcNoArg4b(6, 12))
+  println(funcNoArg4b(20, 25))
 }
